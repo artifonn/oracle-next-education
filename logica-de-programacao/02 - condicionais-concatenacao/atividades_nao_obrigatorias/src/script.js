291e9eb1e4c3aa7cb5c0d00bf751907def123373
Sérgio Artifon <artifonn@gmail.com>
2024-01-17 15:53:26 -0300

    feat: Adicionar resolução das atividades não obrigatórias

---
+++ logica-de-programacao/02 - condicionais-concatenacao/atividades_nao_obrigatorias/src/script.js
@@ -0,0 +1,47 @@
+// 1 - Pergunte ao usuário qual é o dia da semana.
+// Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". 
+// Caso contrário, mostre "Boa semana!".
+
+  // while(true){
+  //   let diaSemana = prompt('Informe o dia da semana: ').toLocaleLowerCase();
+
+  //   if(diaSemana == 'sábado' || diaSemana == 'domingo') {
+  //     alert('Bom fim de semana!');
+  //   } else {
+  //       alert('Boa semana!');
+  //   }
+  // }
+
+// 2 - Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+
+  // let numeroDigitado = parseInt(prompt('Digite um número: '));
+
+  // if(numeroDigitado >= 0) {
+  //   alert(`O número ${numeroDigitado} é positivo!`);
+  // } else {
+  //   alert(`O número ${numeroDigitado} é negativo!`);
+  // }
+
+// 3 - Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, 
+// mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+
+  // let pontuacao = prompt('Informe uma pontuação: ');
+
+  // if(pontuacao >= 100) {
+  //   alert('Parabéns, você venceu!')
+  // } else {
+  //   alert('Tente novamente para ganhar.');
+  // }
+
+// 4 - Crie uma mensagem que informa o usuário sobre o saldo da conta, 
+// usando uma template string para incluir o valor do saldo.
+
+    // let saldoDaConta = parseFloat(prompt('Informe o saldo da conta: ')).toFixed(2);
+    // alert(`O salda do conta é:  R$${saldoDaConta}`);
+
+// 5 - Peça ao usuário para inserir seu nome usando prompt. Em seguida, 
+// mostre um alerta de boas-vindas usando esse nome.
+
+  // let nome = prompt('Infome o seu nome: ');
+  // alert(`Boas vindas ${nome}`);
+
