2aacb89c2054b3f2b0b2320a26131126f4b483e7
Sérgio Artifon <artifonn@gmail.com>
2024-01-17 12:11:47 -0300

    refactor: Reestruturação das pastas do repositório

---
+++ logica-de-programacao/01 - iniciando_com_js/logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+
+let senhaDoSistema = "senhaTeste!";
+
+let senha = prompt("Digite a senha do sistema:");
+
+if (senha = senhaDoSistema){
+    alert(senha);
+} else {
+  alert('Acesso negado!');
+}
