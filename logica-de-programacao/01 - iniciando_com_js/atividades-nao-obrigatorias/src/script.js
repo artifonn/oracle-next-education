d92e3a2c854b04f1f6bbe403ea33877055aa79b3
Sérgio Artifon <artifonn@gmail.com>
2024-01-17 12:09:26 -0300

    refactor: Reestruturação das pastas do repositório

---
+++ logica-de-programacao/01 - iniciando_com_js/atividades-nao-obrigatorias/src/script.js
@@ -0,0 +1,54 @@
+// 1 - Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+
+  alert('Boas vindas ao nosso site!');  
+
+// 2 - Declare uma variável chamada nome e atribua a ela o valor "Lua".
+
+  // var nome = 'Lua';
+  // alert('Nome informado: ' + nome);
+
+// 3 - Crie uma variável chamada idade e atribua a ela o valor 25.
+
+  // var idade = 25;
+  // alert('Idade: ' + idade);
+
+
+// 4 - Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+
+  // var numeroDeVendas = 50;
+  // alert('Total de vendas: ' + numeroDeVendas);
+
+// 5 - Defina uma variável saldoDisponivel e atribua a ela o valor 1000. 
+
+  // var saldoDisponivel = 1000;
+  // alert(`Saldo disponivel: US$${saldoDisponivel}`);
+
+// 6 - Exiba um alerta com o texto "Erro! Preencha todos os campos.";
+
+  // alert('Erro! Preencha todos os campos.');
+
+// 7 - Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos." 
+//  Agora exiba um alerta com o valor da variável mensagemDeErro.
+
+  // var mensagemDeErro = 'Erro! Preencha todos os campos.';
+  // alert(mensagemDeErro);
+
+// 8 - Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
+
+  // var nome = prompt('Informe o seu nome: ');
+  // alert('Olá, ' + nome +'!');
+
+// 9 - Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+
+    // let idade = prompt('Informe a sua idade: ');
+    // alert('A idade informada é: ' + idade);
+
+// 10 - Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+
+    // var idade = prompt('infomre a sua idade: ');
+
+    // if(idade >= 18) {
+    //   alert('Pode tirar a habilitação!');
+    // } else {
+    //   alert('Menor de idade!');
+    // }
