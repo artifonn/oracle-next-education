150444d1bfa17b47217fc119a40c9f9ebf2502f5
Sérgio Artifon <artifonn@gmail.com>
2024-01-20 11:36:00 -0300

    feature: Resolução das atividades não obrigatórias do curso de logica de programação funções e listas aula quatro

---
+++ logica-funcoes-listas/04_listas/atividades_nao_obrigatorias/README.md
@@ -0,0 +1,4 @@
+# Nesta aula:
+
+- Aprendemos a melhorar o código existente, tornando-o mais eficiente e legível. 
+- Neste caso, refatoramos a função que realiza o sorteio do número secreto, tornando-a mais robusta e garantindo que o número sorteado não seja repetido.
---
+++ logica-funcoes-listas/04_listas/atividades_nao_obrigatorias/script.js
@@ -0,0 +1,31 @@
+// 1 - Crie uma lista vazia, com o nome listaGenerica.
+
+  let listaGenerica = ['Java', 'Ruby', 'GoLang'];
+  console.log(listaGenerica);
+
+// 2 - Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 
+// 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+
+  // let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+  // console.log(linguagensDeProgramacao);
+
+// 3 - Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+ 
+// linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+  // console.log(linguagensDeProgramacao);
+
+// 4 - Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+ 
+// let listaDeNomes = ['João', 'Pedro', 'Maria'];
+  // console.log(listaDeNomes[0]);
+
+// 5 - Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+
+  // let listaDeNomes = ['João', 'Pedro', 'Maria'];
+  // console.log(listaDeNomes[1]);
+
+// 6- Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+
+  // let listaDeNomes = ['João', 'Pedro', 'Maria'];
+  // console.log(listaDeNomes[listaDeNomes.length - 1]);
+
