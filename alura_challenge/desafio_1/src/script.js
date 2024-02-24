8c544d0dba3c43e6bc9dc70bfc01525e7e256bc2
Sérgio Artifon <artifonn@gmail.com>
2024-02-24 17:00:05 -0300

    feature(js): Finalizar a implementação do codigo javaScript

--- alura_challenge/desafio_1/src/script.js
+++ alura_challenge/desafio_1/src/script.js
@@ -1,52 +1,76 @@
-function criptografar() {
-  const textoParaCriptografar = document.getElementById('area__input').value;
-  const chaveCifra = gerarChave();
-  
-  let textoCifrado = '';
-  
-  for (let i = 0; i < textoParaCriptografar.length; i++) {
-    const caractereOriginal = textoParaCriptografar[i].toUpperCase();
-    const caractereCifrado = chaveCifra[caractereOriginal] || caractereOriginal;
-    textoCifrado += caractereCifrado;
+const botao__criptografar = document.querySelector('.botao__criptografar');
+const botao__descriptografar = document.querySelector('.botao__descriptografar');
+const sectionn__area__retangulo__imagem = document.querySelector('.sectionn__area__retangulo__imagem');
+const section__area__texto = document.querySelector('.section__area__texto');
+const section__resultado = document.querySelector('.section__resultado');
+const section__area__resultado = document.querySelector('.section__area__resultado');
+const section__area__botao__copiar = document.querySelector('.section__area__botao__copiar');
+const botao__copiar = document.querySelector('.copiar');
+const botao__limpar = document.querySelector('.limpar');
+const mensagem = 'Peço desculpas, parece que você não inseriu um texto para ser criptografado. Por favor, forneça o texto desejado, e ficarei feliz em ajudar com a criptografia.';
+
+botao__criptografar.onclick = criptografarTexto;
+botao__descriptografar.onclick = descriptografarTexto;
+section__area__botao__copiar.onclick = copiar;
+botao__limpar.onclick = limpar;
+
+const chave = 'AluraOracle';
+
+function criptografarTexto() {
+  const textoParaCriptografar = document.querySelector('.area__input').value;
+  if (textoParaCriptografar !== '') {
+    const textoCriptografado = CryptoJS.AES.encrypt(textoParaCriptografar, chave);
+
+    sectionn__area__retangulo__imagem.style.display = 'none';
+    section__area__texto.style.display = 'none';
+    section__area__resultado.style.height = '781px';
+    
+    section__resultado.innerHTML = textoCriptografado;
+    botao__copiar.style.display = 'block';
+    botao__limpar.style.display = 'none';
+  } else {
+    sectionn__area__retangulo__imagem.style.display = 'none';
+    section__area__texto.style.display = 'none';
+    section__area__resultado.style.height = '781px';
+    section__resultado.innerHTML = mensagem;
+    botao__limpar.style.display = 'block';
   }
-  
-  exibirResultado(textoCifrado);
 }
 
-function gerarChave() {
-  const alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  const alfabetoEmbaralhado = embaralharAlfabeto([...alfabeto]);
-  const chaveEmbaralhada = {};
+function descriptografarTexto() {
+  const textoParaDescriptografar = document.querySelector('.area__input').value;
+  if (textoParaDescriptografar !== '') {
+    const textoDescriptografado = CryptoJS.AES.decrypt(textoParaDescriptografar, chave).toString(CryptoJS.enc.Utf8);
 
-  for (let i = 0; i < alfabeto.length; i++) {
-    chaveEmbaralhada[alfabeto[i]] = alfabetoEmbaralhado[i];
+    sectionn__area__retangulo__imagem.style.display = 'none';
+    section__area__texto.style.display = 'none';
+    
+    section__area__resultado.style.height = '781px';
+    section__resultado.innerHTML = textoDescriptografado;
+    botao__copiar.style.display = 'none';
+    botao__limpar.style.display = 'block';
+  } else {
+    sectionn__area__retangulo__imagem.style.display = 'none';
+    section__area__texto.style.display = 'none';
+    section__area__resultado.style.height = '781px';
+    section__resultado.innerHTML = mensagem;
+    botao__limpar.style.display = 'block';
   }
-
-  return chaveEmbaralhada;
 }
 
-function embaralharAlfabeto(arrayAlfabeto) {
-  for (let i = arrayAlfabeto.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arrayAlfabeto[i], arrayAlfabeto[j]] = [arrayAlfabeto[j], arrayAlfabeto[i]];
-  }
-
-  return arrayAlfabeto;
+function copiar() {
+  const textoCopiado = document.querySelector('.section__resultado').textContent;
+  navigator.clipboard.writeText(textoCopiado);
 }
 
-function exibirResultado(textoCifrado) {
-  if(textoCifrado !== '') {
-    const area__texto = document.querySelector('.section__area__texto');
-    const area__imagem = document.querySelector('.sectionn__area__retungulo__imagem');
-    area__texto.style.display = 'none';
-    area__imagem.style.display = 'none';
-
-    const areaResultado = document.querySelector('.section__resultado');
-    let teste = areaResultado.innerHTML = textoCifrado;
-
-    const section__area__botao__copiar = document.querySelector('.section__area__botao__copiar');
-    section__area__botao__copiar.style.display = 'flex';
-  }
+function limpar() {
+  const inputTexto = document.querySelector('.area__input');
+  sectionn__area__retangulo__imagem.style.display = 'block';
+  section__area__texto.style.display = 'block';
+  section__resultado.innerHTML = '';
+  section__area__resultado.style.height = '0px';
+  botao__limpar.style.display = 'none';
+  inputTexto.value = '';
 }
 
 
